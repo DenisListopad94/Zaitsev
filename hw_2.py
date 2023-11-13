59a574142c07f9228599f2f35917b420fcc75f49
Denis <denis.zaitsev18@outlook.com>
2023-11-13 22:40:40 +0100

    hw_2

---
+++ hw_2.py
@@ -0,0 +1,59 @@
+#task 1
+# fruit = "Watermelon"
+# print(fruit[3])
+# print(fruit[8])
+# print(fruit[:6])
+# print(fruit[:8])
+# print(fruit[2:10:2])
+# print(fruit[1:10:2])
+# print(fruit[::-1])
+# print(fruit[::-2])
+# print(len(fruit))
+
+#task 2
+# vegetables = "potato"
+# print(vegetables.startswith("p"))
+# print(vegetables.endswith("p"))
+
+# #task 3
+# pet = "hamster"
+# print(pet[1:4])
+
+#task 4
+# apple = "яблоко"
+# print(apple[1:5])
+# print(apple[-3:])
+
+#task 5
+# full_name = "Ivanou Ivan"
+# surname = full_name.rfind(" ")
+# name = full_name.find(" ")
+# print(full_name[surname:],full_name[:name])
+
+#task 6
+# small_house = "шалаш"
+# print(small_house[::-1])
+
+#task 7
+# animals = ["Wolf","Bear","Hare","Bird"]
+# print(animals)
+# animals.pop(2)
+# print(animals)
+
+#task 8
+# str1 = "employ"
+# str2 = "employment"
+# print(f"{str1} contains {str2} = {str1 in str2}")
+
+# task 9
+# work = 'office'
+# print(work.find('f')+1)
+
+#task 10
+# school = {'1A': 25, '2B': 16, '3C': 17, '4D': 18, '5A': 20, '6B': 19, "C": 18, '8D': 19, '9A': 27, '10B': 17}
+# school['2B'] = 23
+# school['9C'] = 15
+# del school['1A']
+# school_9_klass = {'9A':27, '9C':15}
+# print(school)
+# print(sum(school_9_klass.values()))
